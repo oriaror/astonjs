d4b73ad5fc097509473a06f6c31198e2c17ae3eb
oriaror <farraxovfarraxov@gmail.com>
2024-02-26 17:27:36 +0400

    message

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(213);
+const obj = { item: "some value" };
+
+function logger() {
+  console.log(`I output only external context: ${this.item}`);
+}
+
+logger.apply(obj);
+logger.call(obj);
+const logger1 = logger.bind(obj);
+logger1();
+
+function bind(fn, thisObj) {
+  return function () {
+    return fn.apply(thisObj, arguments);
+  };
+}
+
+const obj1 = bind(logger, obj);
+obj1();
